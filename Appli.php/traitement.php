fbe69127dd3c8f1da105217cbdd865c41d78a544
davinahouquet <davina.houquet@hotmail.com>
2023-04-05 10:31:26 +0200

    Création fichier CSS

--- Appli.php/traitement.php
+++ Appli.php/traitement.php
@@ -3,9 +3,9 @@
 
 if(isset($_POST['submit'])){            //Limite l'accès à traitrement.php ppar les seules requêtes HTTP de la soumission du formulaire
 
-    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_STRING); //Supprime caractères spéciaux + balises HTML
+    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_SPECIAL_CHARS); //Supprime caractères spéciaux + balises HTML
     $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT,FILTER_FLAG_ALLOW_FRACTION); //Valide prix que si virgule ou point
-    $qtt = filter_input(INPUT_POST,"qtt", FILTER_VALIDATE_INT); //Valide quantité que si c'est un entier différent de zéro
+    $qtt = filter_input(INPUT_POST,"qtt", FILTER_SANITIZE_SPECIAL_CHARS); //Valide quantité que si c'est un entier différent de zéro
 
     if($name && $price && $qtt){ //Vérifie que les données sont nettoyées (valeur jugée positive par php)
 
@@ -13,10 +13,9 @@ if(isset($_POST['submit'])){            //Limite l'accès à traitrement.php ppa
                 "name" => $name,
                 "price" => $price,
                 "qtt" => $qtt,
-                "total" => $price *qtt
+                "total" => $price *$qtt
             ];
-
-            $_SESSION['product'][] = $product; //Doit aussi être un tableau pour pouvoir y stocker de nvx produits
+            $_SESSION['products'][] = $product; //Doit aussi être un tableau pour pouvoir y stocker de nvx produits
     }
 }
 header("Location:index.php");
