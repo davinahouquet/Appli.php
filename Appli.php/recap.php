6c2f7bbf69ef2fdf0272c53d6bd4cd4e4a3c0267
davinahouquet <davina.houquet@hotmail.com>
2023-04-05 09:30:48 +0200

    if/else isset et foreach récp

--- Appli.php/recap.php
+++ Appli.php/recap.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+    session_start(); //Récupère la session corresponsant à l'utilisateur
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +10,36 @@
     <title>Récapitualtif des produits</title>
 </head>
 <body>
-    
+    <?php 
+        if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
+            echo "<p>Aucun produit en session...</p>";
+        }
+        else {
+            echo "<table>", //Tableau HTML pour bien décomposer les données de chaque produit
+                    "<thead>",
+                        "<tr>",
+                            "<th>#</th>",
+                            "<th>Nom</th>",
+                            "<th>Prix</th>",
+                            "<th>Quantité</th>",
+                            "<th>Total</th>",
+                        "</tr>",
+                    "</thead>",
+                    "<tbody>";
+
+            foreach($_SESSION['products'] as $index => $product){      //Pour chaque donnée dans $_SESSION['product'] il y a 2 variable dans la boucle
+                echo "<tr>",
+                        "<td>".$index."</td>",                         //aura pour valeur l'index du tabl $_SESSION['product']parcouru.
+                        "<td>".$product['name']."</td>",               //$product contiendra le produit sous forme de tabl, tel que créé et stocké en session fich traitement.php
+                        "<td>".$product['price']."</td>",
+                        "<td>".$product['qtt']."</td>",
+                        "<td>".$product['total']."</td>",
+                    "</tr>";
+            }
+            echo "</tbody>",
+                "</table>";
+        }
+    ?>
+
 </body>
 </html>
--- Appli.php/traitement.php
+++ Appli.php/traitement.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+    session_start();
 
 if(isset($_POST['submit'])){            //Limite l'accès à traitrement.php ppar les seules requêtes HTTP de la soumission du formulaire
 
