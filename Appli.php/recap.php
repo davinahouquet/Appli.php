4a14676c54b2e59a0ca404e8476c0eab25c69dd0
davinahouquet <davina.houquet@hotmail.com>
2023-04-05 16:57:40 +0200

    Reste à faire

--- Appli.php/recap.php
+++ Appli.php/recap.php
@@ -33,12 +33,14 @@
   <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
 </svg></li>
   <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-  
+
   <?php 
-  //$i = 0;
-  //foreach($_SESSION['products'] as $product){
-   // $i++;
-   // } 
+  // Resta à faire : faire le foreach qui affichera la qtt de produit dans le badge
+
+    $i = 0;
+    foreach($_SESSION['products'] as $index => $product){
+    $i++;
+} 
     ?>
 
     <span class="visually-hidden">unread messages</span>
