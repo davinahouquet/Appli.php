ac2a6213e23ac898c66e7cbc6b57b8d8616c2c12
davinahouquet <davina.houquet@hotmail.com>
2023-04-05 14:26:29 +0200

    Navbar dark

--- Appli.php/index.php
+++ Appli.php/index.php
@@ -10,9 +10,22 @@
         <title>Ajout produit</title>
     </head>
     <body>
-    <nav class="nav">
-    <a class="nav-link active" aria-current="page" href="http://localhost/Appli.php/Appli.php/index.php/">Accueil</a>
-    <a class="nav-link active" aria-current="page" href="http://localhost/Appli.php/Appli.php/recap.php">Récapitulatif</a>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" href="http://localhost/Appli.php/Appli.php/index.php/">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/Appli.php/Appli.php/recap.php/">Récapitulatif</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
             <h1>Ajouter un produit</h1>
             <form action="traitement.php" method="post">
--- Appli.php/recap.php
+++ Appli.php/recap.php
@@ -11,11 +11,23 @@
 
 </head>
 <body>
-<nav class="nav">
-    <a class="nav-link active" aria-current="page" href="http://localhost/Appli.php/Appli.php/index.php">Accueil</a>
-    <a class="nav-link active" aria-current="page" href="http://localhost/Appli.php/Appli.php/recap.php">Récapitulatif</a>
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="http://localhost/Appli.php/Appli.php/index.php/">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="http://localhost/Appli.php/Appli.php/recap.php/">Récapitulatif</a>
+        </li>
+      </ul>
+    </div>
+  </div>
 </nav>
-
         <h1>Récapitulatif des produits</h1>
     <?php 
         if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
