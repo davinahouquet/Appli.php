e30210889648ab7dae48710482d394b1710efd1a
davinahouquet <davina.houquet@hotmail.com>
2023-04-05 09:40:28 +0200

    $totalGeneral (récapitualtif)

--- Appli.php/recap.php
+++ Appli.php/recap.php
@@ -26,17 +26,22 @@
                         "</tr>",
                     "</thead>",
                     "<tbody>";
-
+            $totalGeneral = 0;
             foreach($_SESSION['products'] as $index => $product){      //Pour chaque donnée dans $_SESSION['product'] il y a 2 variable dans la boucle
                 echo "<tr>",
                         "<td>".$index."</td>",                         //aura pour valeur l'index du tabl $_SESSION['product']parcouru.
                         "<td>".$product['name']."</td>",               //$product contiendra le produit sous forme de tabl, tel que créé et stocké en session fich traitement.php
-                        "<td>".$product['price']."</td>",
+                        "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
                         "<td>".$product['qtt']."</td>",
-                        "<td>".$product['total']."</td>",
-                    "</tr>";
+                        "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    "</tr>";                                          //La boucle créera ligne <tr>+ toutes les cellules <td> nécessaire à chaque partie du produit à afficher
+                $totalGeneral += $product['total'];
             }
-            echo "</tbody>",
+            echo "<tr>",
+                    "<td colspan = 4>Total général : </td>",
+                    "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                    "</tr>",
+                    "</tbody>",
                 "</table>";
         }
     ?>
