3f0e10c37d499e889770741fd0d010e65db17e75
davinahouquet <davina.houquet@hotmail.com>
2023-04-06 10:59:05 +0200

    Ajout unset(['message'])

--- Appli.php/index.php
+++ Appli.php/index.php
@@ -78,9 +78,11 @@ Panier</svg></li>
 </div>  
 <?php
 
-$erreur = (isset($_SESSION['message']))?
+$message = (isset($_SESSION['message']))?
 $_SESSION['message']: null;
-echo $erreur;
+echo $message;
+unset($_SESSION['message']);
+
 
 ?>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>      
--- Appli.php/recap.php
+++ Appli.php/recap.php
@@ -41,7 +41,7 @@
     $totalGeneral += $product['qtt'];
 }  
   echo $totalGeneral;
-  
+
     ?>
 
   </span>
@@ -84,6 +84,20 @@
                     "</tbody>",
                 "</table>";
         }
+
+
+    //-------------------Supprimer un produit en session-----------------------------
+
+
+
+
+    //-------------------Supprimer tous les produits en session en une seule fois----
+
+
+
+
+    //------------------Modifier les quantités de chaque produit---------------------
+    
     ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </div>
