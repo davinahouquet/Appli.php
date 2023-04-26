19a5425a1806f517db522f076b5f56f6a5c03c08
davinahouquet <davina.houquet@hotmail.fr>
2023-04-26 12:02:05 +0200

    Correction indentation

--- Appli.php/index.php
+++ Appli.php/index.php
@@ -1,101 +1,50 @@
-<?php
+<!-- L'important est de garder le "coeur" de la page. = ce qui change !
+Ce qui ne change pas va dans le template -->
+
+
+<!-- ob_start commence l'enregistrement -->
+<?php 
+
+ob_start()
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-        <title>Ajout produit</title>
-    </head>
-    <body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" href="http://localhost/Appli.php/Appli.php/index.php">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/Appli.php/Appli.php/recap.php">Récapitulatif</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <a href="http://localhost/Appli.php/Appli.php/recap.php">
- <button type="button" class="btn btn-dark position-relative">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-bag-fill" viewBox="0 0 16 16">
-  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
-Panier</svg></li>
-  <span class="position-static top-0 start-100 translate-middle badge rounded-pill bg-danger">
-    
-  <?php 
-  
-  session_start();
-  
-  //Pour afficher la quantité de produit dans le panier
-  if(empty($_SESSION['products'])){
-    echo "0";
-  } else {
-  $totalGeneral = 0;
-  foreach($_SESSION['products'] as $index => $product){
-  $totalGeneral += $product['qtt'];
-} echo $totalGeneral;
-  }
-  ?>
 
-  </span>
-</button>
-</a>
-</nav>
 <div class="container-sm">
-            <h1>Ajouter un produit</h1>
-            <form action="traitement.php?action=addProduct" method="post">
-                <p>
-                    <label>
-                        Nom du produit :
-                        <input type="text" name="name">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Prix du produit :
-                        <input type="number" step="any" name="price">
-                    </label>
-                </p>
-                <p>
-                    <label>
-                        Quantité désirée :
-                        <input type="number" name="qtt" value="1">
-                    </label>
-                </p>
-                <p>
-                  <label>Description :
-                    <textarea type="text" name="details"  rows="1" cols="25"></textarea>
-                  </label>
-                </p>
-                <p>
-                    <input type="submit" name="submit" value="Ajouter le produit">
-                </p>
-            </form>    
+  <h1>Ajouter un produit</h1>
+    <form action="traitement.php?action=addProduct" method="post">
+      <p>
+        <label>Nom du produit :
+          <input type="text" name="name">
+        </label>
+      </p>
+      <p>
+        <label>Prix du produit :
+          <input type="number" step="any" name="price">
+        </label>
+      </p>
+      <p>
+        <label>Quantité désirée :
+          <input type="number" name="qtt" value="1">
+        </label>
+      </p>
+      <p>
+        <label>Description :
+          <textarea type="text" name="details"  rows="1" cols="25"></textarea>
+        </label>
+      </p>
+      <p>
+        <input type="submit" name="submit" value="Ajouter le produit">
+      </p>
+    </form>    
 </div>  
+
+
 <?php
+// ob_get_clean arrête l'enregistrement et stocke les infos dans la variable $contenu
+$contenu = ob_get_clean();
 
-$message = (isset($_SESSION['message']))?
-$_SESSION['message']: null;
-echo $message;
-unset($_SESSION['message']);
+// $titre est dans le template
+$titre = "Ajout produit";
 
-
-?>
-
-           
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-        </body>
-</html>
-
+// require est comme une une fonction copier/coller du template
+require "template.php";
--- Appli.php/recap.php
+++ Appli.php/recap.php
@@ -1,123 +1,70 @@
-<?php
-    session_start(); //Récupère la session corresponsant à l'utilisateur
+<?php 
+
+ob_start() 
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Récapitualtif des produits</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href="style.css">
-    
-    
-</head>
-<body>
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-  <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="http://localhost/Appli.php/Appli.php/index.php">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="http://localhost/Appli.php/Appli.php/recap.php">Récapitulatif</a>
-</li>
-      </ul>
-    </div>
-  </div>
- <a href="http://localhost/Appli.php/Appli.php/recap.php">
-  <button type="button" class="btn btn-dark position-relative">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-bag-fill" viewBox="0 0 16 16">
-  <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
-</svg></li>
-  <span class="position-static top-0 start-100 translate-middle badge rounded-pill bg-danger">
 
-  <?php 
-  //if le panier est rempli sinon 
-  if(empty($_SESSION['products'])){
-    echo "0";
-  } else {
-    $totalGeneral = 0;
-    foreach($_SESSION['products'] as $index => $product){
-    $totalGeneral += $product['qtt'];
-}  
-  echo $totalGeneral;
-  }
-    ?>
-
-  </span>
-</button>
-</a>
-</nav>
 <div class="container-sm">
-        <h1>Récapitulatif des produits</h1>
+  <h1>Récapitulatif des produits</h1>
+
     <?php 
-        if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
-            echo "<p>Aucun produit en session...</p>";
-        }
-        else {
-            echo "<table class='table container'>", //Tableau HTML pour bien décomposer les données de chaque produit
-                    "<thead>",
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                            "<th> </th>",
-                        "</tr>",
-                    "</thead>",
-                    "<tbody>";
-            $totalGeneral = 0;
-            foreach($_SESSION['products'] as $index => $product){      //Pour chaque donnée dans $_SESSION['product'] il y a 2 variable dans la boucle
-                echo "<tr>",
-                        "<td>".$index."</td>",                         
-                        "<td><a href='traitement.php?action=details&index=$index'><button type='button' class='btn btn-light' data-toggle='modal' data-target='#productModal'>".$product['name']."</button></a>
-
-                        <div id='productModal' class='modal fade' role='dialog'>
-                          <div class='modal-dialog'>
-
-                            <div class='modal-content'>
-                              <div class='modal-header'>
-                                <button type='button' class='close' data-dismiss='modal'>&times;</button>
-                                <h4 class='modal-title'>".$product['name']."</h4>
-                              </div>
-                              <div class='modal-body'>
-                                <p>".$product['details']."</p>
-                              </div>
-                              <div class='modal-footer'>
-                                <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
-                              </div>
-                            </div>
-                          
-                          </div>
+      if(!isset($_SESSION['products']) || empty($_SESSION['products'])){
+        echo "<p>Aucun produit en session...</p>";
+      }
+      else {
+        echo "<table class='table container'>", //Tableau HTML pour bien décomposer les données de chaque produit
+               "<thead>",
+                "<tr>",
+                 "<th>#</th>",
+                  "<th>Nom</th>",
+                  "<th>Prix</th>",
+                  "<th>Quantité</th>",
+                  "<th>Total</th>",
+                  "<th> </th>",
+                "</tr>",
+              "</thead>",
+              "<tbody>";
+        $totalGeneral = 0;
+      foreach($_SESSION['products'] as $index => $product){ //Pour chaque donnée dans $_SESSION['product'] il y a 2 variable dans la boucle
+        echo "<tr>",
+              "<td>".$index."</td>",                         
+                "<td><a href='traitement.php?action=details&index=$index'><button type='button' class='btn btn-light' data-toggle='modal' data-target='#productModal'>".$product['name']."</button></a>
+                  <div id='productModal' class='modal fade' role='dialog'>
+                    <div class='modal-dialog'>
+                      <div class='modal-content'>
+                        <div class='modal-header'>
+                          <button type='button' class='close' data-dismiss='modal'>&times;</button>
+                            <h4 class='modal-title'>".$product['name']."</h4>
                         </div>
-                        </td>
-                        <td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                        "<td><a href='traitement.php?action=decreaseProduct&index=$index'><button type='button' class='btn btn-outline-dark'>  -  </button></a>   ".$product['qtt']."   <a href='traitement.php?action=increaseProduct&index=$index'><button type='button' class='btn btn-outline-dark'> + </button></a></td>",
-                        "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                        "<td><a href='traitement.php?action=delete&index=$index'><button type='button' class='btn-close' aria-label='Close'></button></a>";
-                    "</tr>";                                          //La boucle créera ligne <tr>+ toutes les cellules <td> nécessaire à chaque partie du produit à afficher
+                        <div class='modal-body'>
+                          <p>".$product['details']."</p>
+                        </div>
+                        <div class='modal-footer'>
+                          <button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                    </td>
+                    <td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    "<td><a href='traitement.php?action=decreaseProduct&index=$index'><button type='button' class='btn btn-outline-dark'>  -  </button></a>   ".$product['qtt']."   <a href='traitement.php?action=increaseProduct&index=$index'><button type='button' class='btn btn-outline-dark'> + </button></a></td>",
+                    "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    "<td><a href='traitement.php?action=delete&index=$index'><button type='button' class='btn-close' aria-label='Close'></button></a>";
+            "</tr>"; //La boucle créera ligne <tr>+ toutes les cellules <td> nécessaire à chaque partie du produit à afficher
                 $totalGeneral += $product['total'];
-            }
-            echo "<tr>",
-                    "<td colspan = 4><strong>Total général : </strong></td>",
-                    "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
-                    "<td><a href='traitement.php?action=deleteAll'><button type='button' class='btn-close' aria-label='Close'></button></a></td>",
-                    "</tr>",
-                    "</tbody>",
-                "</table>";
+      }
+        echo "<tr>",
+                "<td colspan = 4><strong>Total général : </strong></td>",
+                  "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                  "<td><a href='traitement.php?action=deleteAll'><button type='button' class='btn-close' aria-label='Close'></button></a></td>",
+              "</tr>",
+              "</tbody>",
+              "</table>";
                 
-            }
-        
-       ?>
-       
-</div>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-</body>
-</html>
+      }
+
+$contenu = ob_get_clean();
+$titre = "Recapitulatif";
+require "template.php";
+
+?>
